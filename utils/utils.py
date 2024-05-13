d5ab259725c70ab6628ec04741a713d3c4130adb
L1ght94 <108329446+L1ght94@users.noreply.github.com>
2024-05-13 12:57:22 +0900

    Add utils directory for utility functions
    Added utility functions like saving and loading pickle file storing
    code snippets, sanitizing inputs etc.

---
+++ utils/utils.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+
+path_to_pickle = os.environ['SNIPPETS_FILE']
+
+def get_headers():
+    headers = {
+        "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+        "Content-Type": "application/json"
+    }
+    return headers
+
+def load_pkl():
+    if not os.path.isfile(path_to_pickle):
+        return []
+
+    with open(path_to_pickle, 'rb') as pklfile:
+        return pickle.load(pklfile)
+
+def update_pkl(snippets):
+    with open(path_to_pickle, 'wb') as pklfile:
+        pickle.dump(snippets, pklfile)
+
+def sanitize_input(input_string):
+    return input_string.replace(";", "").replace("&", "")
