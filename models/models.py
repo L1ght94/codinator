452ef64679ec4a555bf03ec835e9b3d25a06ebb0
L1ght94 <108329446+L1ght94@users.noreply.github.com>
2024-05-13 11:56:53 +0900

    Add models directory
    Added models directory storing pydantic models for FastAPI

---
+++ models/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class Snippet(BaseModel):
+    id: Optional[int] = None
+    language: str
+    description: str
+    code: Optional[str] = None
+    feedback: Optional[str] = None
+    previous_messages: Optional[List[dict]] = []
+
+class Feedback(BaseModel):
+    snippet_id: int
+    message: str
