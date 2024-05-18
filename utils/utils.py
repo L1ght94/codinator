283c8341d0180e88be345d46f6818d25995680ec
L1ght94 <108329446+L1ght94@users.noreply.github.com>
2024-05-18 10:41:41 +0900

    Add more utility functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,17 @@
 import os
 import pickle
+import openai
 
 path_to_pickle = os.environ['SNIPPETS_FILE']
 os.makedirs(os.path.dirname(path_to_pickle), exist_ok=True)
+
+def get_openai_api_key():
+    return os.getenv("OPENAI_API_KEY")
+
+
+def get_openai_model():
+    return os.getenv("OPENAI_MODEL")
+
 
 def get_headers():
     headers = {
@@ -25,8 +34,31 @@ def update_pkl(snippets):
 def sanitize_input(input_string: str) -> str:
     return input_string.replace(";", ".").replace("&", "and")
 
+def find_languages(text: str) -> list[str]:
+    messsage = {
+        "role": "user",
+        "content": f"""Extract the names of the programming languages from the
+        following text:\n\n{text}"""
+    }
+    
+    response = openai.chat.completions.create(
+        model=get_openai_model(),
+        messages=[messsage],
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0
+    )
+
+    languages = response.choices[0].message.content.strip().split(',')
+    languages = [language.strip() for language in languages]
+
+    return languages
+
 def extract_code_snippet(output_str: str) -> str:
     try:
-        return f"```{output_str.split('```')[1]}```"
+        if '```' in output_str:
+            output_str = f"{output_str.split('```')[1]}"
+        return output_str
     except:
         raise Exception("Could not get code snippet")
